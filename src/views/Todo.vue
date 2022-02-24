d42cde945bfe1f85ccb732b8cf354cadac0ddd1d
Abraham Peter <theclassical001@gmail.com>
2022-02-24 19:22:31 +0100

    delete icon added + updated

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -5,15 +5,22 @@
       flat
     >
     <div v-for="task in tasks" :key="task.id">
-        <v-list-item @click="doneTask(task.id)">
+        <v-list-item @click="doneTask(task.id)" :class="{ 'blue lighten-5' : task.done}">
         <template v-slot:default>
             <v-list-item-action>
             <v-checkbox :input-value="task.done"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-            <v-list-item-title>{{ task.title}}</v-list-item-title>
+            <v-list-item-title :class="{ 'text-decoration-line-through' : task.done}">
+                {{ task.title}}
+            </v-list-item-title>
             </v-list-item-content>
+            <v-list-item-action>
+                <v-btn icon>
+                    <v-icon color="primary">mdi-delete</v-icon>
+                </v-btn>
+            </v-list-item-action>
         </template>
         </v-list-item>
         <v-divider></v-divider>
